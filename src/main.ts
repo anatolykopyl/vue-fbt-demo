b079ea5484da44bf40018fc611da43572e9e6be2
anatolykopyl <hi@kopyl.dev>
2024-04-23 21:28:23 +0300

    Add types

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { init } from 'fbt'
+import {init, IntlVariations, type IntlViewerContext} from 'fbt'
 
 import translations from './translatedFbts.json'
 import App from './App.vue'
 
+const viewerContext: IntlViewerContext = {
+  locale: 'en_US',
+  GENDER: IntlVariations.GENDER_UNKNOWN,
+}
+
 init({
   translations,
   hooks: {
-    getViewerContext: () => ({locale: 'en_US'}),
+    getViewerContext: () => viewerContext,
   }
 })
 
